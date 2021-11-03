858f50ba305043cdd63528c4c475484e2cc6b57f
jeff <jeffk713@gmail.com>
2021-11-03 16:53:01 -0700

    complete assignment

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require('net');
+const { setupInput } = require('./input');
 
 // establishes a connection with the game server
 const connect = function () {
@@ -16,12 +17,11 @@ const connect = function () {
 
   conn.on('connect', () => {
     conn.write('Name: Q1Q');
-
+    conn.write('Say: BS');
     // conn.write('Move: up');
     // setTimeout(() => conn.write('Move: up'), 100);
     // setInterval(() => conn.write('Move: up'), 200);
   });
-
   conn.on('end', () => console.log('kicked out from idling too long'));
 
   return conn;
--- input.js
+++ input.js
@@ -1,18 +1,35 @@
+let connection;
+
 const handleUserInput = function (key) {
   // your code here
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+
 };
 
 // setup interface to handle user input from stdin
-exports.setupInput = function () {
+exports.setupInput = function (conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
 
   stdin.on('data', handleUserInput);
+
+  connection = conn;
 
   return stdin;
 };
